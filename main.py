f5e8a551eb967f4daa88fd45ab7857bfe97fb935
ViktorJJF <vj.jimenez96@gmail.com>
2025-03-19 09:49:27 -0500

    Update: added headers to simulate human when open page

--- main.py
+++ main.py
@@ -58,7 +58,22 @@ class PlaywrightManager:
         """Create a new browser context."""
         if not self.browser:
             raise RuntimeError("Browser not started")
-        self.global_context = await self.browser.new_context()
+        self.global_context = await self.browser.new_context(
+            user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36",
+            extra_http_headers={
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+                "Accept-Language": "en-US,en;q=0.5",
+                "Accept-Encoding": "gzip, deflate, br",
+                "DNT": "1",
+                "Connection": "keep-alive",
+                "Upgrade-Insecure-Requests": "1",
+                "Sec-Fetch-Dest": "document",
+                "Sec-Fetch-Mode": "navigate",
+                "Sec-Fetch-Site": "none",
+                "Sec-Fetch-User": "?1",
+                "Cache-Control": "max-age=0",
+            }
+        )
         return self.global_context
     
     async def take_screenshot(self, url: str):
